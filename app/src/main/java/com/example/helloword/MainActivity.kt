31ea874f47b82af87a3c660afe49ffd065896b32
youseuf <yuzma852@gmail.com>
2022-10-04 12:21:51 +0900

    menambah projek kedua

--- app/src/main/java/com/example/helloword/MainActivity.kt
+++ app/src/main/java/com/example/helloword/MainActivity.kt
@@ -1,43 +1,60 @@
-package com.example.helloword
+package com.unmus.androidtraining
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.helloword.ui.theme.HelloWordTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HelloWordTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            val user = User("Unmus", "Papua", 30)
+            MenampilkanPesan(user)
         }
     }
 }
 
+data class User(
+    val name: String,
+    val address: String,
+    val umur: Int
+)
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MenampilkanPesan(user: User){
+    Row() {
+        Image(painter =
+        painterResource(id = R.drawable.logo),
+            contentDescription = "Uni musamus",
+            modifier = Modifier
+                // Set image size to 40 dp
+                .size(100.dp)
+                // Clip image to be shaped as a circle
+                .clip(CircleShape)
+        )
+        Column() {
+            Text(text = user.name)
+            Text(text = user.umur.toString())
+            Text(text = user.address)
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
-    HelloWordTheme {
-        Greeting("Android")
-    }
+fun PreviewMenampilkanPesan(){
+    val user = User("Unmus", "Papua", 30)
+    MenampilkanPesan(user)
 }
